fa3755ebb5fbfc7fd6b3fbc85dce8d01b61eff4e
Brian Stamm <bkstamm67@gmail.com>
2018-01-31 14:03:47 -0800

    updated comments, added variables

--- session2/assignment2/test/domain/SkillTests.java
+++ session2/assignment2/test/domain/SkillTests.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.scg.domain;
 
 import static org.junit.Assert.*;
@@ -8,19 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
+ * Tests the Skill enum class
  * @author Brian Stamm
- *
  */
 public class SkillTests {
 
+	//Variables for testing
+	private int testRate = 150;
+	private String softwareTesterString = "Tester";
+	
+	/**
+	* Tests the getRate() method, checking that it returns the correct value
+	*/
 	@Test
 	public void testGetRate() {
-		assertEquals(Skill.SOFTWARE_ENGINEER.getRate(),150);
+		assertEquals(Skill.SOFTWARE_ENGINEER.getRate(),testRate);
 	}
 	
+	/**
+	* Tests the toString() method
+	*/
 	@Test
 	public void testToString() {
-		assertEquals(Skill.SOFTWARE_TESTER.toString(),"Tester");
+		assertEquals(Skill.SOFTWARE_TESTER.toString(),softwareTesterString);
 	}
 
 }
