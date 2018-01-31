3e16c1b288a99b660d29094796decda3c547a320
Brian Stamm <bkstamm67@gmail.com>
2018-01-31 12:42:51 -0800

    update PersonalName test by adding comments

--- session2/assignment2/test/util/PersonalNameTests.java
+++ session2/assignment2/test/util/PersonalNameTests.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.scg.util;
 
 import static org.junit.Assert.*;
@@ -8,36 +5,48 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
+ * This tests PersonalName.
  * @author Brian Stamm
- *
  */
 public class PersonalNameTests {
-
+	
+	//Private variables used for testing
 	private String lastName = "Cicero";
 	private String firstName = "Marcus";
 	private String middleName = "Tullius";
 	private String partName = lastName + ", " + firstName;
 	private String fullName = lastName + ", " + firstName + " " + middleName;
 	
-	
+	/**
+	* Tests constructor taking two arguments.
+	*/
 	@Test
 	public void testTwoParameterConstructor() {
 		PersonalName tester = new PersonalName(lastName, firstName);
 		assertEquals(tester.toString(), partName);
 	}
-
+	
+	/**
+	* Tests constructor taking three arguments.
+	*/
 	@Test
 	public void testThreeParametterConstructor() {
 		PersonalName tester = new PersonalName(lastName, firstName, middleName);
 		assertEquals(tester.toString(), fullName);
 	}
 	
+	/**
+	* Test method getLastName()
+	*/
 	@Test
 	public void testGetLastName() {
 		PersonalName tester = new PersonalName(lastName, firstName, middleName);
 		assertEquals(tester.getLastName(),lastName);
 	}
 	
+	/**
+	* Test method setLastName(), passing new value in
+	*/
 	@Test
 	public void testSetLastName() {
 		PersonalName tester = new PersonalName();
@@ -45,12 +54,18 @@ public class PersonalNameTests {
 		assertEquals(tester.getLastName(),lastName);
 	}
 	
+	/**
+	* Test method getFirstName()
+	*/
 	@Test
 	public void testGetFirstName() {
 		PersonalName tester = new PersonalName(lastName, firstName, middleName);
 		assertEquals(tester.getFirstName(),firstName);
 	}
 	
+	/**
+	* Test method setFirstName(), passing new value in
+	*/
 	@Test
 	public void testSetFirstName() {
 		PersonalName tester = new PersonalName();
@@ -58,12 +73,18 @@ public class PersonalNameTests {
 		assertEquals(tester.getFirstName(),firstName);
 	}
 	
+	/**
+	* Test method getMiddleName()
+	*/
 	@Test
 	public void testGetMiddleName() {
 		PersonalName tester = new PersonalName(lastName, firstName, middleName);
 		assertEquals(tester.getMiddleName(),middleName);
 	}
 	
+	/**
+	* Test method setMiddleName(), passing new value in
+	*/
 	@Test
 	public void testSetMiddleName() {
 		PersonalName tester = new PersonalName();
@@ -71,6 +92,9 @@ public class PersonalNameTests {
 		assertEquals(tester.getMiddleName(),middleName);
 	}
 	
+	/**
+	* Test method equal(), with two PersonalName that are the same and one different
+	*/
 	@Test
 	public void testEquals() {
 		PersonalName testerOne = new PersonalName(lastName, firstName, middleName);
