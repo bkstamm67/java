408da83fcdb9182350290ef318db52eb952c7b03
Brian Stamm <bkstamm67@gmail.com>
2018-01-31 12:51:03 -0800

    updated clientAccountTest, adding comments

--- session2/assignment2/test/domain/ClientAccountTests.java
+++ session2/assignment2/test/domain/ClientAccountTests.java
@@ -7,14 +7,18 @@ import org.junit.Test;
 import com.scg.util.PersonalName;
 
 /**
+ * These are the tests for the ClientAccount class
  * @author Brian Stamm
- *
  */
 public class ClientAccountTests {
 
+	//variables used for testing
 	PersonalName actualClient = new PersonalName("Bazelon", "Emily");
 	String actualName = "Slate Gabfest";
 	
+	/**
+	* Tests the constructor
+	*/
 	@Test
 	public void testConstructor() {
 		ClientAccount tester = new ClientAccount(actualName,actualClient);
@@ -22,12 +26,18 @@ public class ClientAccountTests {
 		assertEquals(tester.getName(),actualName);
 	}
 	
+	/**
+	* Tests the method isBillable()
+	*/
 	@Test
 	public void testIsBillable() {
 		ClientAccount tester = new ClientAccount(actualName,actualClient);
 		assertEquals(tester.isBillable(), true);
 	}
 	
+	/**
+	* Tests the method setContact(), passing in a new PersonalName value
+	*/
 	@Test
 	public void testSetContact() {
 		ClientAccount tester = new ClientAccount(actualName,actualClient);
