89246294e27d0c20ae91ac049948edaf7d90f359
Brian Stamm <bkstamm67@gmail.com>
2018-01-31 10:07:30 -0800

    Create tirial.java
    seeing how this works

---
+++ session1/geo_shape/trial.java
@@ -0,0 +1,12 @@
+/**
+ * Encapsulates the first, middle and last name of a person.
+ * @author Brian Stamm
+ *
+ */
+public class PersonalName {
+	
+	private String lastName;
+	private String firstName;
+	private String middleName;
+  
+  }
