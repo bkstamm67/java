98c5056756fa660f6d142394a204badcdfac893d
Brian Stamm <bkstamm67@gmail.com>
2018-01-31 14:00:17 -0800

    Updated the comments

--- session2/assignment2/test/domain/NonBillableAccountTests.java
+++ session2/assignment2/test/domain/NonBillableAccountTests.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.scg.domain;
 
 import static org.junit.Assert.*;
@@ -8,24 +5,34 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
+ * Unit tests for the NonBillableAccount class
  * @author Brian Stamm
- *
  */
 public class NonBillableAccountTests {
 	
+	//Variables used for testing
 	private String sick = "Sick Leave";
 	private String vaca = "Vacation";
 
+	/**
+	* Tests the toString() method
+	*/
 	@Test
 	public void testToString() {
 		assertEquals(NonBillableAccount.SICK_LEAVE.toString(),sick);
 	}
 	
+	/**
+	* Tests the getName() method
+	*/
 	@Test
 	public void testGetName() {
 		assertEquals(NonBillableAccount.VACATION.getName(),vaca);
 	}
 	
+	/**
+	* Tests that NonBillableAccount's method isBillable() returns false
+	*/
 	@Test
 	public void testIsBillable() {
 		assertFalse(NonBillableAccount.BUSINESS_DEVELOPMENT.isBillable());
