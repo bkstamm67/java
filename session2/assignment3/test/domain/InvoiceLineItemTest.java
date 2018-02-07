188dcae4279b9e8430607596a81817eb13258d09
Brian Stamm <bkstamm67@gmail.com>
2018-02-07 08:56:25 -0800

    created tests

--- session2/assignment3/test/domain/InvoiceLineItemTest.java
+++ session2/assignment3/test/domain/InvoiceLineItemTest.java
@@ -11,16 +11,51 @@ import org.junit.Test;
  */
 public class InvoiceLineItemTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private LocalDate currentDate;
+	private Consultant testConsultant;
+	private Skill testSkill;
+	private int testHours;
+	private int testCharge;
+	private String testString;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUp(){
+		currentDate = LocalDate.now();
+		testConsultant = new Consultant(new PersonalName("Miklasz","Bernie")));
+		testSkill = Skill.PROJECT_MANAGER;
+		testHours = 13;
+		testCharge = testHours * testSkill.getRate();
+		
+		StringBuilder sb = new StringBuilder();
+		Formatter ft = new Formatter(sb);
+		ft.format("%1$tm/%1$td/%1$tY  %2$s\t%3$s\t\t%4$d\t$%5$,.2f\n", currentDate, testConsultant.toString(),testSkill.toString(),testHours,(double)testCharge);
+		ft.close();
+		testString = sb.toString();
 	}
 
-	@Test
-	public void test() {
-		assertEquals(1,1);
+	@SuppressWarnings("unused")
+	@Test(expected = IllegalArgumentException.class)
+	public void testConstructorNegativeHours(){
+		InvoiceLineItem testLineItem = new InvoiceLineItem(currentDate,testConsultant,testSkill,-2);
 	}
+	
+	@Test
+	public void testConstructor() {
+		InvoiceLineItem testLineItem = new InvoiceLineItem(currentDate,testConsultant,testSkill,testHours);
+		
+		assertEquals(testLineItem.getDate(),currentDate);
+		assertEquals(testLineItem.getConsultant(),testConsultant);
+		assertEquals(testLineItem.getSkill(),testSkill);
+		assertEquals(testLineItem.getHours(),testHours);
+		assertEquals(testLineItem.getCharge(),testCharge);
+	}
+	
+	@Test
+	public void testToString() {
+		InvoiceLineItem testLineItem = new InvoiceLineItem(currentDate,testConsultant,testSkill,testHours);
+		
+		assertEquals(testLineItem.toString(),testString);
+	}
+
 
 }
