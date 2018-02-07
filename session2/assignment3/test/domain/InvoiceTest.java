0435a7bdf6f073353fea4ddd1d353e1a17de4a3f
Brian Stamm <bkstamm67@gmail.com>
2018-02-07 09:53:26 -0800

    started on tests, still need to complete

--- session2/assignment3/test/domain/InvoiceTest.java
+++ session2/assignment3/test/domain/InvoiceTest.java
@@ -11,16 +11,37 @@ import org.junit.Test;
  */
 public class InvoiceTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private List<ClientAccount> testClients;
+	private List<Consultant> testConsultants;
+	private List<TimeCard> testTimeCards;
+	LocalDate aDate;
+
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
+		testClients = new ArrayList<>();
+		testConsultants = new ArrayList<>();
+		testTimeCards = new ArrayList<>();
+		ListFactory.populateLists(testClients,testConsultants,testTimeCards);
+		aDate = LocalDate.of(2009,1,1);
 	}
 
 	@Test
-	public void test() {
-		assertEquals(1,1);
+	public void testConstructor() {
+		Invoice testInvoice = new Invoice(testClients.get(0),aDate.getMonth(),aDate.getYear());
+		
+		assertEquals(testInvoice.getStartDate(),aDate);
+		assertEquals(testInvoice.getInvoiceMonth(),aDate.getMonth());
+		assertEquals(testInvoice.getClientAccount(),testClients.get(0));
+		assertEquals(testInvoice.getTotalHours(),0);
+		assertEquals(testInvoice.getTotalCharges(),0);
 	}
+	
+	@Test
+	public void testAddLineItem(){
+	}
+	
+	//test extractLineItems
+	//test toString
+	//test toReportString
 
 }
