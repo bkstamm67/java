2169b6a3ee65dc7aa8eab0c341b5a461493366c4
Brian Stamm <bkstamm67@gmail.com>
2018-02-07 08:20:40 -0800

    updated the test

--- session2/assignment3/test/domain/InvoiceFooterTest.java
+++ session2/assignment3/test/domain/InvoiceFooterTest.java
@@ -10,17 +10,37 @@ import org.junit.Test;
  *
  */
 public class InvoiceFooterTest {
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+	
+	private String testDashes = "=========================================================================\n";
+	private String testBusiness = "Expeditors";
 
 	@Test
-	public void test() {
-		assertEquals(1,1);
+	public void testConstructorToString() {
+		StringBuilder sb = new StringBuilder();
+		Formatter ft = new Formatter(sb);
+		ft.format("%s\nPage Number: %d\n%s\n", testBusiness,1,testDashes);
+		ft.close();
+		String testString = sb.toString();
+		
+		InvoiceFooter footer = new InvoiceFooter(testBusiness);
+		assertEquals(footer.toString(),testString);
+	}
+	
+	@Test
+	public void testIncrementPageNumber(){
+		StringBuilder sb = new StringBuilder();
+		Formatter ft = new Formatter(sb);
+		ft.format("%s\nPage Number: %d\n%s\n", testBusiness,5,testDashes);
+		ft.close();
+		String testString = sb.toString();
+		
+		InvoiceFooter footer = new InvoiceFooter(testBusiness);
+		
+		for(int i =0; i<4;i++){
+			footer.incrementPageNumber();
+		}
+		
+		assertEquals(footer.toString(),testString);
 	}
 
 }
